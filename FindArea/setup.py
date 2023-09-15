ded9d9d69bbc43d9d6ff0152df9b26bb8c55fc96
susmp <116519843+susmp@users.noreply.github.com>
2023-09-15 16:57:36 +0300

    Add files via upload

---
+++ FindArea/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+ 
+setup(name='AreaFinding',
+      version='0.1',
+      url='https://github.com/susmp/test_task.git',
+      license='MIT',
+      author='Pavlov M',
+      author_email='mspavlovhse@gmail.com',
+      description='Find are of different figures',
+      packages=find_packages(),
+      )
