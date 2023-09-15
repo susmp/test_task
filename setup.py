2b567ee5170e9a7620287fa793b3de11750e172a
Maxim <mspavlovhse@gmail.com>
2023-09-15 15:42:11 +0300

    123

--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@ from setuptools import setup
 
 setup(
     name='area_search',
-    version='0.0.1',
+    version='0.1',
     description='Нахождение площади геометрических фигур',
-    license='MIT',
-    packages=['area_search'],
+    packages=['FindArea'],
     author='Maxim P',
     author_email='mspavlovhse@gmail.com',
-    keywords=['sis'],
-    url='https://github.com'
+    url='https://github.com/susmp/test_task.git'
 )
